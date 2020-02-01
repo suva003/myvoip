40ce04bb57ee5546211d1c6bad0e80bd7278c464
SaptarshiDas <saptarshi.das.1104@gmail.com>
2020-02-02 02:36:54 +0530

    File Changed

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShopComponent } from './shop/shop.component';
 import { BusinessvoipComponent } from './businessvoip/businessvoip.component';
+import { HomeComponent } from "./home/home.component";
+import { ErrorComponent } from "./error/error.component";
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component : HomeComponent},
   {path: 'shop', component: ShopComponent},
-  {path: 'shop', component: ShopComponent},
-  {path:'businessvoip', component: BusinessvoipComponent}
+  {path:'businessvoip', component: BusinessvoipComponent},
+  {path: '**', component: ErrorComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true } )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
